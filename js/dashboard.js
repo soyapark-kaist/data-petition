46c1d6fced545279b51050af9aa3e8a4b73362e3
soyapark-kaist <soya@mit.edu>
2018-07-25 23:06:23 -0400

    Load signatures

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,8 +8,23 @@ function signupSuccess() {
     'requestMask.includeField': 'person.names,person.emailAddresses'
   }).then(function(response) {
     appendPre("Hi, " + response.result.names[0].givenName);
-    callScriptFunction('getSignatures', [], displayEditForm);
+
+    var params = getJsonFromUrl(true);
+    if( !params['petition'] ) {
+      alert("This petition not exist!")
+      return;
+    }
+    var formLink = "https://docs.google.com/forms/d/" + params['petition'] + "/edit?usp=sharing";
+    callScriptFunction('getSignatures', [formLink], displaySignatureSummary);
   }, function(reason) {
     console.log('Error: ' + reason.result.error.message);
   });
 }
+
+function displaySignatureSummary(inRes) {
+  console.log(inRes);
+
+  $("#participants-number").text(inRes.length);
+
+  showLoader(false);
+} 
